5ec283984ec827e2f63169af11c4b5b04ac56633
lorant-v <88926792+lorant-v@users.noreply.github.com>
2025-11-27 11:18:46 +0100

    Complete unicafe 1.6 - 1.11

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td> 
+    </tr>
+  )
+}
+
+
+const Statistics = ({good, neutral, bad}) => {
+  const sumAll = good + neutral + bad
+  const average = (good - bad) / sumAll
+  const positive = ((good / sumAll) * 100) + ' %'
+
+
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+       <h1>statistics</h1>
+       <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={sumAll} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
