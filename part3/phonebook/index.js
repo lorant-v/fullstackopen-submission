b70c857a33a7d0f9497c523c0c03622cda13790d
lorant-v <88926792+lorant-v@users.noreply.github.com>
2025-12-09 19:54:20 +0100

    Complete 3.1 - 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const MAX_ID = 1_000_000
+
+  return getRandomInt(MAX_ID)
+}
+
+app.post('/api/persons', (req, res) => {
+
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "Name missing"
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: "Number missing"
+    })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({
+      error: "Name must be unique"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+app.get('/info', (req, res) => {
+  content = `
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`
+
+  res.send(content)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
